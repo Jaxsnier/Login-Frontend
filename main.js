534f87b9f3c148a6169c47f08e46242cd7c20d92
ELIGIO <eligioestevez@hotmail.com>
2025-09-08 00:00:24 -0400

    creando proyecto

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = ()=>{
+    console.log("Página cargada");
+    console.log("Hello, World! desde main.js");
+
+}
+
+const Login = () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log("Email:", username);
+    }
