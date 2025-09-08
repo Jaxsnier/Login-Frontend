a6fc4b48ab1486b5996356b0a096787304522bff
ELIGIO <eligioestevez@hotmail.com>
2025-09-08 17:06:58 -0400

    agregando estilos al login y trabajando loguearse

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-window.onload = ()=>{
-    console.log("Página cargada");
-    console.log("Hello, World! desde main.js");
+window.onload = () => {
+    let ruta = 'login'; //registro o home
+    let urlogin = 'https://login-ten-sigma-39.vercel.app/auth/login';
 
-}
+    const loginForm = document.getElementById('login-form');
 
-const Login = () => {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();                 // Evita el envío del formulario
 
-    console.log("Email:", username);
-    }
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        try {
+
+            fetch(urlogin, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password })
+            }).then(response => response.json())  //error si no es json cuando usuario o pass incorrecto usar 
+                .then(data => {
+                    console.log('Success:', data);
+                    console.log("Datos enviados al servidor");
+                })
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    })
+};
